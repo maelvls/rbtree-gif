66ca975cb1131392d987c339ed2ce90ee323e848
Maël Valais <mael.valais@gmail.com>
2021-01-05 15:50:10 +0100

    dot_to_gif: reformat using shfmt

--- dot_to_gif.sh
+++ dot_to_gif.sh
@@ -1,35 +1,18 @@
 #! /usr/bin/env bash
 #
-# (MIT license)
-# Copyright 2013 Mael Valais <mael.valais@gmail.fr>
+# Create a GIF using a set of DOT files.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
+# Copyright (C) 2013-2020  Mael Valais
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
 
 OUT=animation.gif
-TIME=100 # in milliseconds
+TIME=100               # in milliseconds
 OPT_RM_INTERMEDIATES=1 # 0 = no
 WATERMARK=""
 LOOP=0 # The resulting GIF should loop (0=no)
 
 function help() {
-cat <<EOF
+  cat <<EOF
 Usage: $(basename $0) INPUT [INPUT...] [-o FILE] [-t TIMEMS] [options]
 
 Description;
@@ -51,30 +34,30 @@ Author:
 EOF
 }
 
-if ! which dot > /dev/null; then
-cat <<EOF >&2
+if ! which dot >/dev/null; then
+  cat <<EOF >&2
 Error: 'dot' not found, Graphviz is probably not installed.
 To install it on macOS:
     brew install graphviz
 On ubuntu:
-    sudo apt-get install graphviz
+    sudo apt install graphviz
 EOF
-	exit 1
+  exit 1
 fi
-if ! which convert > /dev/null; then
-cat <<EOF >&2
+if ! which convert >/dev/null; then
+  cat <<EOF >&2
 Error: 'convert' not found, ImageMagick is probably not installed.
 To install it on macOS:
     brew install imagemagick
 On ubuntu:
-    sudo apt-get install imagemagick
+    sudo apt install imagemagick
 EOF
-	exit 1
+  exit 1
 fi
 
 if [ $# -eq 0 ]; then
-	help | head -1 >&2
-	exit 1
+  help | head -1 >&2
+  exit 1
 fi
 
 # Nb of dot files
@@ -82,64 +65,66 @@ nb_inputs=0
 inputs=
 
 while [ "$1" ]; do
-	case "$1" in
-		"--keep" | "-k")
-			OPT_RM_INTERMEDIATES=1;;
-		"--help"|"-h")
-			help
-			;;
-		"-l")
-			LOOP=1
-			;;
-		"-o" ) # Name of the gif file
-			if [ "$2" ]; then
-				OUT="$2"
-				shift
-			else
-				echo "Error: -o needs a file path" >&1
-				help | head -1 >&2
-				exit 1
-			fi
-			;;
-		"-t" ) # Time (in ms) between images
-			if [ "$2" ]; then
-				TIME="$2"
-				shift
-			else
-				echo "Error: -t needs an argument" >&1
-				help | head -1 >&2
-				exit 1
-			fi
-			;;
-		"-w" ) # Watermark in GIF
-			if [ "$2" ]; then
-				WATERMARK="$2"
-				shift
-			else
-				echo "Error: -w needs an argument" >&1
-				help | head -1 >&2
-				exit 1
-			fi
-			;;
-		-? | --*)
-			echo "Error: unknown flag $1" >&2
-			exit 1
-			;;
-		*)
-			(( nb_inputs++ ))
-			inputs[$nb_inputs]="$1"
-			intermediates[$nb_inputs]="$1.png"
-	esac
-	shift
+  case "$1" in
+  "--keep" | "-k")
+    OPT_RM_INTERMEDIATES=1
+    ;;
+  "--help" | "-h")
+    help
+    ;;
+  "-l")
+    LOOP=1
+    ;;
+  "-o") # Name of the gif file
+    if [ "$2" ]; then
+      OUT="$2"
+      shift
+    else
+      echo "Error: -o needs a file path" >&1
+      help | head -1 >&2
+      exit 1
+    fi
+    ;;
+  "-t") # Time (in ms) between images
+    if [ "$2" ]; then
+      TIME="$2"
+      shift
+    else
+      echo "Error: -t needs an argument" >&1
+      help | head -1 >&2
+      exit 1
+    fi
+    ;;
+  "-w") # Watermark in GIF
+    if [ "$2" ]; then
+      WATERMARK="$2"
+      shift
+    else
+      echo "Error: -w needs an argument" >&1
+      help | head -1 >&2
+      exit 1
+    fi
+    ;;
+  -? | --*)
+    echo "Error: unknown flag $1" >&2
+    exit 1
+    ;;
+  *)
+    ((nb_inputs++))
+    inputs[$nb_inputs]="$1"
+    intermediates[$nb_inputs]="$1.png"
+    ;;
+  esac
+  shift
 done
 
 if [ $nb_inputs -eq 0 ]; then
-	echo "Error: missing FILE [FILE...]" >&2
-	help | head -1 >&2
-	exit 1
+  echo "Error: missing FILE [FILE...]" >&2
+  help | head -1 >&2
+  exit 1
 fi
 
-cat <<"EOF" > /tmp/script.dot
+cat <<"EOF" >/tmp/script.dot
 // script par Emden R. Gansner
 // trouve sur http://stackoverflow.com/questions/10902745/enforcing-horizontal-node-ordering-in-a-dot-tree
 
@@ -221,23 +206,23 @@ size_max_x=0
 size_max_y=0
 
 for fichier in ${inputs[@]}; do
-	echo "Processing $fichier"
-	dot "$fichier" | gvpr -c -f/tmp/script.dot | neato -n -Tpng -o "${fichier}.png"
-	size_temp=`identify "${fichier}.png" | cut -d " " -f 3`
-	size_temp_x=`echo $size_temp | cut -d 'x' -f1`
-	size_temp_y=`echo $size_temp | cut -d 'x' -f2`
+  echo "Processing $fichier"
+  dot "$fichier" | gvpr -c -f/tmp/script.dot | neato -n -Tpng -o "${fichier}.png"
+  size_temp=$(identify "${fichier}.png" | cut -d " " -f 3)
+  size_temp_x=$(echo $size_temp | cut -d 'x' -f1)
+  size_temp_y=$(echo $size_temp | cut -d 'x' -f2)
 
-	if [ "$size_temp_x" -gt "$size_max_x" ]; then size_max_x=$size_temp_x; fi
-	if [ "$size_temp_y" -gt "$size_max_y" ]; then size_max_y=$size_temp_y; fi
+  if [ "$size_temp_x" -gt "$size_max_x" ]; then size_max_x=$size_temp_x; fi
+  if [ "$size_temp_y" -gt "$size_max_y" ]; then size_max_y=$size_temp_y; fi
 done
 
 echo "Generation of ${OUT} (size: ${size_max_x}x${size_max_y})"
 
 convert -delay ${TIME} -loop ${LOOP} -extent ${size_max_x}x${size_max_y} -dispose Background -background white ${intermediates[@]} ${OUT}
-(( size_max_x=$size_max_x-90 ))
-((size_max_y=$size_max_y-12))
+((size_max_x = $size_max_x - 90))
+((size_max_y = $size_max_y - 12))
 convert -pointsize 14 -fill red -draw "text $size_max_x,$size_max_y '${WATERMARK}'" ${OUT} ${OUT}
 
 if [ $OPT_RM_INTERMEDIATES -eq 1 ]; then
-	rm ${intermediates[@]}
+  rm ${intermediates[@]}
 fi
